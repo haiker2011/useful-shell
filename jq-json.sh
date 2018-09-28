6fc930048fb3ad97c1d4bf67d1ed863e672cc963
孙海洲 <haizhou.uestc2011@gmail.com>
2018-09-28 19:05:21 +0800

    Update jq-json.sh

--- jq-json.sh
+++ jq-json.sh
@@ -1,4 +1,11 @@
 # 下载安装jq
 sudo apt install jq
+
 # 提取json数据中nc_news_url和cont字段
 jq -c '{nc_news_url:.nc_news_url,cont:.cont}' test.txt > newtest.txt
+
+# 格式化输出
+jq -r . test.txt
+
+# 输出id字段
+echo ${test.txt} |jq -r '.id'
